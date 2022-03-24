f621ba2ab30df96ddd85247d4fd8a84ee4f98e76
1000zoo <cjswldn99@gmail.com>
2022-03-24 23:11:01 +0900

    .

--- policies/policy1_v_0_2_3.py
+++ policies/policy1_v_0_2_3.py
@@ -2,10 +2,10 @@ from decimal import DivisionByZero
 import pyupbit as pu
 import time
 import datetime
-
 import excelog
 from ___key___ import Access_Key, Secrete_Key
 
+BTC="KRW-BTC"
 SEC=1
 MIN=60*SEC
 HOUR=60*MIN
@@ -13,16 +13,16 @@ DAY=24*HOUR
 
 BFEE = 0.9995
 FEE = 0.999
-BTC="KRW-BTC"
+ADJ_FEE = 1.0005
 
-TARGET = 1.0063
+TARGET = 1.015
 LOSS = 0.9755
+COMPARE = 0.9834
 
-COMPARE = 0.9852
+ADJ_T = 0.9997
+ADJ_L = 1.0003
+ADJ_C = 1.0008
 
-ADJ_T = 0.99993
-ADJ_L = 1.00001
-ADJ_C = 1.00342
 data = {
     "time" : [],
     "trade type" : [],
@@ -33,6 +33,7 @@ data = {
     "comp" : [],
     "current balance" : []
 }
+
 def main():
     up = pu.Upbit(Access_Key, Secrete_Key)
     timer = time.time()
@@ -46,7 +47,7 @@ def main():
     comp = COMPARE
     print_with_timestemp("start at ")
 
-    while time.time() - timer < 3*HOUR:
+    while time.time() - timer < 10*HOUR:
         try:
             curr_price = pu.get_current_price(BTC)
             if can_buy(up.get_balance()):
@@ -71,10 +72,10 @@ def main():
                 if past_time(reset_timer):
                     target *= ADJ_T
                     loss *= ADJ_L
+                    reset_timer = time.time()
                 if hold_range(avg_price, curr_price, target, loss):
                     sell(up, avg_price, curr_price)
                     print(target, loss)
-                    reset_timer = time.time()
                     min_price = pu.get_current_price(BTC)
                     try:
                         data["target"].append(target)
@@ -84,6 +85,7 @@ def main():
                         print(err)
                     target = TARGET
                     loss = LOSS
+                    reset_timer = time.time()
             else:
                 print("no money & coin")
                 break
@@ -91,9 +93,9 @@ def main():
         except KeyboardInterrupt as err:
             print(err)
             break
-        
+
     print(data)
-    excelog.excelog(data, title="excelog-" + excelog.datetime_to_title(start_time))
+    excelog.excelog(data, title=excelog.datetime_to_title(start_time), path="excelog_files/")
     print_with_timestemp("end at ")
     print("initial balance: " + str(init_bal))
     print("final balance: " + str(up.get_balance()))
@@ -105,7 +107,7 @@ def can_sell(v):
     return v > 9000
 
 def hold_range(avg, curr, t, l):
-    t = max(t, 1/FEE)
+    t = max(t, 1/FEE*ADJ_FEE)
     l = min(l, 1)
     return t*avg < curr or curr < l*avg
 
@@ -166,7 +168,7 @@ def print_time():
     print(datetime.datetime.now())
 
 def past_time(rtime):
-    return time.time() - rtime > 3*MIN
+    return time.time() - rtime > 0.5*MIN
 
 def sell_condition(up):
     return 
