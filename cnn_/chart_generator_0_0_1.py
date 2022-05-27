9a9001ab8ef569524e75b7383f81f80bd1e38408
1000zoo <cjswldn99@gmail.com>
2022-05-27 20:35:28 +0900

    chart generator test

---
+++ cnn_/chart_generator_0_0_1.py
@@ -0,0 +1,189 @@
+import pyupbit as pu
+from datetime import datetime
+import random
+
+import mplfinance as mpf
+from PIL import Image
+
+import time
+import os
+"""
+target data set:
+        * 봉 종류: 1min~60min
+        * 총 8개의 종류로 각 500장씩
+        * 총 4000장 (장당 20KB 정도, 총 72MB 정도)
+
+function:
+    * for load candle;
+        - get_ohlcv
+        - generate_to
+        - now_toformat
+        - to_formate
+        - my_format
+    
+    * for generate & crop & classify;
+        - generate_images
+"""
+## 경로설정
+PATH = "/Users/1000zoo/Documents/prog/data_files/ccat_data/"
+TEST = PATH + "test/"
+VAL = PATH + "val/"
+TRAIN = PATH + "train/"
+DEC = "decrease/"
+INC = "increase/"
+SID = "sideways/"
+
+## 퍼센테이지 설정
+INCPER = 1.0025
+DECPER = 0.9965
+
+## 봉 수 설정
+TCOUNT = 41
+
+## 캔들 pandas data 가져오기
+def get_ohlcv(ticker="KRW-BTC", count=TCOUNT):
+    M = "minute"
+    interval_list = [1, 5, 10, 15, 30, 60]
+
+    while True:
+        il = random.choice(interval_list)
+        interval = M + str(il)
+        candle = pu.get_ohlcv(ticker, count=count, interval=interval, to=generate_to())
+        yield candle
+
+## 기준일 랜덤 생성기
+def generate_to():
+    year = random.randint(2018,2022)
+    ## 윤년 처리
+    ## 추후 윤년을 일반화하는 코드 및 함수 추가
+    if year == 2020:
+        Feb = 29
+    else:
+        Feb = 28
+    day_max = [31,Feb,31,30,31,30,31,31,30,31,30,31]
+    month = my_random(12, 1)
+    day = my_random(day_max[month-1], 1)
+    hour = my_random(24)
+    minute = my_random(60)
+    tmp = to_format(
+        year, month, day, hour, minute
+    )
+    # 현재보다 높은 시간대가 나오면 무시
+    if int(tmp) > int(now_toformat()):
+        generate_to()
+    else:
+        return tmp
+
+## 월, 일, 시, 분 랜덤 생성
+def my_random(x, y=0):
+    return int(random.random()*x) + y
+
+## 현재 시간을 string 형식으로 반환
+def now_toformat():
+    now = datetime.now()
+    return to_format(
+        now.year, now.month, now.day, now.hour, now.minute
+    )
+
+## 입력된 시간을 string 형식으로 변환
+def to_format(year, month, day, hour, minute):
+    to = ""
+    to += my_format(year)
+    to += my_format(month)
+    to += my_format(day)
+    to += my_format(hour)
+    to += my_format(minute)
+    return to
+
+## 한 자리 수를 두 자리 수로 변환
+def my_format(t):
+    if t < 10:
+        return "0" + str(t)
+    else:
+        return str(t)
+
+## main
+def generate_images(count=1000, path=TRAIN, ticker="KRW-BTC"):
+    ## 변수 초기화
+    dcount = icount = scount = 0
+    c = 0
+    tp = ""
+    starttime = time.time()
+
+    ## main loop
+    for candle in get_ohlcv(ticker=ticker):
+        ## 종료 조건문
+        if dcount >= count and icount >= count and scount >= count:
+            break
+        if time.time() - starttime > 600:
+            break
+
+        ## 차트 불러오기 ()
+        colorset = mpf.make_marketcolors(up='r', down='b', volume='blue')
+        s = mpf.make_mpf_style(marketcolors=colorset)
+        mpf.plot(candle[:TCOUNT - 1], type='candle', volume=True, style=s, savefig="train.jpg")
+        mpf.plot(candle[:TCOUNT], type='candle', volume=True, style=s, savefig="full.jpg")
+
+        ## p1: 마지막 가격
+        ## p2: 정답의 고가
+        ## p3: 정답의 저가
+        candle.reset_index(drop=False, inplace=True)
+        p1 = candle.loc[TCOUNT-2]["close"]
+        p2 = candle.loc[TCOUNT-1]["high"]
+        p3 = candle.loc[TCOUNT-1]["low"]
+        ## p2 와 p3 의 내분 값
+        pinc = 0.75*p2 + 0.25*p3
+        pdec = 0.25*p2 + 0.75*p3
+        moveto = path
+
+        ## 최종가격의 INCPER(>1) 배가 pinc보다 낮다면, 상승
+        if p1 * INCPER < pinc:
+            ## 원하는 데이터 수를 채웠으면 스킵
+            if icount < count:
+                moveto += INC
+                icount += 1
+                c = icount
+                tp = INC
+            else:
+                continue
+        ## 최종가격의 DECPER(<1) 배가 정답의 평균가보다 높다면, 하락
+        elif p1 * DECPER > pdec:
+            if dcount < count:
+                moveto += DEC
+                dcount += 1
+                c = dcount
+                tp = DEC
+            else:
+                continue
+        ## 어느쪽에도 해당되지 않는다면 횡보
+        else:
+            if scount < count:
+                moveto += SID
+                scount += 1
+                c = scount
+                tp = SID
+            else:
+                continue
+        
+        ## 학습용 차트와 확인용 차트 저장
+        area = (165, 70, 710, 480)
+        img = Image.open("train.jpg")
+        crop_img = img.crop(area)
+        crop_img.save("crop_train.jpg")
+        fig = "crop_train.jpg"
+        newname = str(c) + ".jpg"
+        os.replace(fig, moveto + newname)
+
+        ## 원본 차트
+        fig = "full.jpg"
+        newname = str(c) + "full.jpg"
+        os.replace(fig, moveto + newname)
+        print(tp, c)
+
+if __name__ == "__main__":
+    print("generate train data")
+    generate_images(1000, TRAIN)
+    print("generate validation data")
+    generate_images(200, VAL)
+    print("generate test data")
+    generate_images(100, TEST)
--- cnn_/chart_generator_base.py
+++ cnn_/chart_generator_base.py
@@ -6,10 +6,9 @@ import mplfinance as mpf
 from PIL import Image
 
 import os
-import copy
 """
 target data set:
-        * day, 1min~240min
+        * 봉 종류: 1min~60min
         * 총 8개의 종류로 각 500장씩
         * 총 4000장 (장당 20KB 정도, 총 72MB 정도)
 
@@ -22,10 +21,9 @@ function:
         - my_format
     
     * for generate & crop & classify;
-
-
-
+        - generate_images
 """
+## 경로설정
 PATH = "/Users/1000zoo/Documents/prog/data_files/ccat_data/"
 TEST = PATH + "test/"
 VAL = PATH + "val/"
@@ -33,10 +31,15 @@ TRAIN = PATH + "train/"
 DEC = "decrease/"
 INC = "increase/"
 SID = "sideways/"
+
+## 퍼센테이지 설정
 INCPER = 1.01
 DECPER = 0.99
+
+## 봉 수 설정
 TCOUNT = 41
 
+## 캔들 pandas data 가져오기
 def get_ohlcv(ticker="KRW-BTC", count=TCOUNT):
     M = "minute"
     interval_list = [1, 5, 10, 15, 30, 60]
@@ -47,9 +50,11 @@ def get_ohlcv(ticker="KRW-BTC", count=TCOUNT):
         candle = pu.get_ohlcv(ticker, count=count, interval=interval, to=generate_to())
         yield candle
 
-
+## 기준일 랜덤 생성기
 def generate_to():
     year = random.randint(2018,2022)
+    ## 윤년 처리
+    ## 추후 윤년을 일반화하는 코드 및 함수 추가
     if year == 2020:
         Feb = 29
     else:
@@ -62,18 +67,20 @@ def generate_to():
     tmp = to_format(
         year, month, day, hour, minute
     )
+    # 현재보다 높은 시간대가 나오면 무시
     if int(tmp) > int(now_toformat()):
         generate_to()
     else:
         return tmp
     
-
+## 현재 시간을 string 형식으로 반환
 def now_toformat():
     now = datetime.now()
     return to_format(
         now.year, now.month, now.day, now.hour, now.minute
     )
 
+## 입력된 시간을 string 형식으로 변환
 def to_format(year, month, day, hour, minute):
     to = ""
     to += my_format(year)
@@ -84,6 +91,7 @@ def to_format(year, month, day, hour, minute):
     return to
 
 
+## 한 자리 수를 두 자리 수로 변환
 def my_format(t):
     if t < 10:
         return "0" + str(t)
@@ -91,19 +99,27 @@ def my_format(t):
         return str(t)
 
 
+## main
 def generate_images(count=1000, p=TRAIN, ticker="KRW-BTC"):
+    ## 변수 초기화
     dcount = icount = scount = 0
     c = 0
     tp = ""
+
+    ## main loop
     for candle in get_ohlcv(ticker=ticker):
         if dcount >= count and icount >= count and scount >= count:
             break
 
+        ## 차트 불러오기 ()
         colorset = mpf.make_marketcolors(up='r', down='b', volume='blue')
         s = mpf.make_mpf_style(marketcolors=colorset)
         chart = mpf.plot(candle[:TCOUNT - 1], type='candle', volume=True, style=s, savefig="test.jpg")
         chart = mpf.plot(candle[:TCOUNT], type='candle', volume=True, style=s, savefig="full.jpg")
 
+        ## p1: 마지막 가격
+        ## p2: 정답의 고가
+        ## p3: 정답의 저가
         candle.reset_index(drop=False, inplace=True)
         p1 = candle.loc[TCOUNT-2]["close"]
         p2 = candle.loc[TCOUNT-1]["high"]
@@ -111,6 +127,7 @@ def generate_images(count=1000, p=TRAIN, ticker="KRW-BTC"):
         moveto = p
 
         if p1 * INCPER < p2:
+            ## 원하는 데이터 수를 채웠으면 스킵
             if icount < count:
                 moveto += INC
                 icount += 1
@@ -135,6 +152,7 @@ def generate_images(count=1000, p=TRAIN, ticker="KRW-BTC"):
             else:
                 continue
         
+        ## 학습용 차트와 확인용 차트 저장
         area = (165, 70, 710, 480)
         img = Image.open("test.jpg")
         crop_img = img.crop(area)
@@ -144,7 +162,6 @@ def generate_images(count=1000, p=TRAIN, ticker="KRW-BTC"):
         newname = str(c) + ".jpg"
         os.replace(fig, moveto + newname)
 
-        area = (165, 70, 710, 480)
         img = Image.open("full.jpg")
         crop_img = img.crop(area)
         crop_img.save("crop_test_full.jpg")
